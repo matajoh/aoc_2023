cbdd4868e748253f010b7cb3d32185acfa2669dc
Matthew A Johnson <matjoh@microsoft.com>
2023-12-03 01:02:49 +0100

    Day 2
    Signed-off-by: Matthew A Johnson <matjoh@microsoft.com>

--- Program.fs
+++ Program.fs
@@ -1,6 +1,7 @@
 ﻿let run i =
     match i with
     | 1 -> Day01.run
+    | 2 -> Day02.run
     | _ -> printfn "Day %i not implemented" i
 
 [<EntryPoint>]
@@ -8,7 +9,7 @@ let main argv =
     printfn "Advent of Code 2023"
     
     match argv.Length with
-    | 0 -> [1..1] 
+    | 0 -> [1..2] 
         |> Seq.iter (fun i -> run i)
     | _ -> argv
         |> Seq.map int
---
+++ day02.fs
@@ -0,0 +1,88 @@
+module Day02
+
+open System
+open System.IO
+
+type Blocks =
+    | Red of int
+    | Green of int
+    | Blue of int
+
+type Handful = Blocks list
+
+type Game = { id: int; handfuls: Handful list }
+
+type Bag = { red: int; green: int; blue: int }
+
+let parseBlocks (s: string) =
+    let parts = s.Trim().Split(' ')
+    let count = int parts.[0]
+
+    match parts.[1] with
+    | "red" -> Red count
+    | "green" -> Green count
+    | "blue" -> Blue count
+    | _ -> failwith "Unknown color"
+
+let parseHandful (s: string) =
+    s.Split(',') |> Seq.map parseBlocks |> Seq.toList
+
+let parseGame (s: string) =
+    let parts = s.Split(':')
+    let gameId = int (parts.[0].Substring(5))
+    let handfuls = parts.[1].Split(';') |> Seq.map parseHandful |> Seq.toList
+    { id = gameId; handfuls = handfuls }
+
+
+let isHandfulPossible bag handful =
+    handful
+    |> List.forall (fun blocks ->
+        match blocks with
+        | Red(c) -> c <= bag.red
+        | Green(c) -> c <= bag.green
+        | Blue(c) -> c <= bag.blue)
+
+let isGamePossible bag game =
+    game.handfuls |> List.forall (isHandfulPossible bag)
+
+let part1 games =
+    let bag = { red = 12; green = 13; blue = 14 }
+    games |> Seq.filter (isGamePossible bag) |> Seq.map (fun g -> g.id) |> Seq.sum
+
+
+let expandBagForBlocks bag blocks =
+    match blocks with
+    | Red(c) -> { bag with red = Math.Max(bag.red, c) }
+    | Green(c) ->
+        { bag with
+            green = Math.Max(bag.green, c) }
+    | Blue(c) ->
+        { bag with
+            blue = Math.Max(bag.blue, c) }
+
+
+let rec expandBagForHandful bag handful =
+    match handful with
+    | [] -> bag
+    | blocks :: rest ->
+        let bag = expandBagForBlocks bag blocks
+        expandBagForHandful bag rest
+
+let minBagFor game =
+    game.handfuls |> List.fold expandBagForHandful { red = 0; green = 0; blue = 0 }
+
+let power bag = bag.red * bag.green * bag.blue
+
+
+let part2 games =
+    games |> Seq.map minBagFor |> Seq.map power |> Seq.sum
+
+
+let run =
+    printfn "== Day 02 =="
+
+    let games = File.ReadLines("inputs/day02.txt") |> Seq.map parseGame |> Seq.toList
+
+    printfn "Part 1: %A" (part1 games)
+    printfn "Part 2: %A" (part2 games)
+    printfn ""
